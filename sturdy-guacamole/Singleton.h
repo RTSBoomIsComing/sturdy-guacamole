7ffae260da96cc4de17059c04440a7fdc80938e3
yudaegam <61501369+yudaegam@users.noreply.github.com>
2023-04-20 18:24:13 +0900

    Add CRTP template class, Singleton

---
+++ sturdy-guacamole/Singleton.h
@@ -0,0 +1,31 @@
+#pragma once
+#include <stdexcept>
+
+template <typename T>
+class Singleton
+{
+protected:
+	inline static T* s_instance{};
+
+public:
+	Singleton()
+	{
+		if (s_instance)
+			throw std::logic_error("Singleton already exists.");
+
+		s_instance = reinterpret_cast<T*>(this);
+	}
+
+	~Singleton()
+	{
+		s_instance = nullptr;
+	}
+
+	static T& Get()
+	{
+		if (!s_instance)
+			throw std::logic_error("Singleton does not exist.");
+
+		return *s_instance;
+	}
+};
