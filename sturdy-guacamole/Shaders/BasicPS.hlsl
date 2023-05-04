b65a19d19851b8dce6dcc542a121c946cef37600
yudaegam <61501369+yudaegam@users.noreply.github.com>
2023-05-04 20:58:36 +0900

    implement PBR

--- sturdy-guacamole/Shaders/BasicPS.hlsl
+++ sturdy-guacamole/Shaders/BasicPS.hlsl
@@ -49,7 +49,7 @@ struct Light
 	float3 color;
 };
 
-const static Light g_light = { {0.0, 2.0, 0.0}, {0.0, -1.0, 0.0}, {1.0, 1.0, 1.0} };
+const static Light g_light = { {0.0, 10.0, 0.0}, {0.0, -1.0, 0.0}, {1.0, 1.0, 1.0} };
 
 struct PSInput
 {
@@ -63,7 +63,7 @@ struct PSInput
 float3 GetNormal(PSInput psInput)
 {
 	float3 n = psInput.normal;
-	
+
 	if (HasNormalTex)
 	{
 		float du1 = ddx(psInput.uv).x;
@@ -106,14 +106,87 @@ float3 GetNormal(PSInput psInput)
 }
 
 
+// The separable form of the Smith joint masking - shadowing function
+float G_Smith(float NdotL, float NdotV, float alpha)
+{
+	float alpha2 = alpha * alpha;
+	float shadowing = 2.0 * NdotL / (NdotL + sqrt(alpha2 + (1.0 - alpha2) * (NdotL * NdotL)));
+	float masking = 2.0 * NdotV / (NdotV + sqrt(alpha2 + (1.0 - alpha2) * (NdotV * NdotV)));
+	return shadowing * masking;
+}
+
+// The Trowbridge-Reitz/GGX microfacet distribution
+float D_GGX(float NdotH, float alpha)
+{
+	float f = (NdotH * NdotH * (alpha * alpha - 1.0) + 1.0);
+	return alpha * alpha / (PI * f * f);
+}
+
+// The specular reflection is a microfacet BRDF
+// microfacet BRDF = G * D / (4 * NdotL * NdotV)
+float3 Specular_brdf(float alpha, float NdotL, float NdotV, float NdotH)
+{
+	return G_Smith(NdotL, NdotV, alpha) * D_GGX(NdotH, alpha) / (4.0 * NdotL * NdotV);
+}
+
+// The diffuse reflection is a Lambertian BRDF
+float3 Diffuse_brdf(float3 color)
+{
+	return (1 / PI) * color;
+}
+
 float4 main(PSInput psInput) : SV_Target0
 {
+	float4 sampledColor = (HasBaseColorTex) ? BaseColorTex.Sample(Sampler_BaseColorTex, psInput.uv) : float4(1.0, 1.0, 1.0, 1.0);
+	sampledColor *= BaseColorFactor;
+
+	float3 baseColor = sampledColor.rgb;
+
+	float metallic = (HasMetallicRoughnessTex) ? MetallicRoughnessTex.Sample(Sampler_MetallicRoughnessTex, psInput.uv).b : 1.0;
+	metallic *= MetallicFactor;
+
+	float roughness = (HasMetallicRoughnessTex) ? MetallicRoughnessTex.Sample(Sampler_MetallicRoughnessTex, psInput.uv).g : 1.0;
+	roughness *= RoughnessFactor;
+
+	float alpha = roughness * roughness;
+
 	float3 n = GetNormal(psInput);
+	float3 v = normalize(ViewerPos - psInput.worldPos);
+	float NdotV = clamp(dot(n, v), 0.001, 1.0);
 
-	float intensity = dot(-g_light.dir, n);
+	float4 color = float4(0.0, 0.0, 0.0, 1.0);
+	// for each light source
+	{
+		float3 l = normalize(g_light.pos - psInput.worldPos);
+		float3 h = normalize(v + l);
 
-	float4 color = (HasBaseColorTex) ? BaseColorTex.Sample(Sampler_BaseColorTex, psInput.uv) : float4(1.0, 1.0, 1.0, 1.0);
-	color *= BaseColorFactor * intensity;
+		float NdotL = clamp(dot(n, l), 0.001, 1.0);
+		float NdotH = clamp(dot(n, h), 0.0, 1.0);
+		float VdotH = clamp(dot(v, h), 0.0, 1.0);
+		//float LdotH = clamp(dot(l, h), 0.0, 1.0);
+
+		float3 metal_brdf = Specular_brdf(alpha, NdotL, NdotV, NdotH) 
+			* (baseColor.rgb + (float3(1.0, 1.0, 1.0) - baseColor.rgb) * pow(1.0 - VdotH, 5));
+
+		float3 dielectric_brdf = lerp(Diffuse_brdf(baseColor.rgb), Specular_brdf(alpha, NdotL, NdotV, NdotH),
+			0.04 + (1.0 - 0.04) * pow(1.0 - VdotH, 5));
+
+		// The BRDF of the metallic-roughness material is a linear interpolation of a metallic BRDF and a dielectric BRDF.
+		float3 material = lerp(dielectric_brdf, metal_brdf, metallic);
+		color.rgb += material;
+
+		// We can simplify the mix and arrive at the final BRDF for the material
+		//float3 c_diff = lerp(baseColor.rgb, float3(0.0, 0.0, 0.0), metallic);
+		//float3 f0 = lerp(float3(0.04, 0.04, 0.04), baseColor.rgb, metallic);
+		//float a = roughness * roughness;
+		//
+		//float3 F = f0 + (1 - f0) * pow(1 - VdotH, 0.5);
+
+		//float3 f_diffuse = (1 - F) / PI * c_diff;
+		//float3 f_specular = F * Specular_brdf(alpha, NdotL, NdotV, NdotH);
+		//float3 material = f_diffuse + f_specular;
+		//color.rgb += material;
+	}
 
 	if (HasOcclusionTex)
 	{
