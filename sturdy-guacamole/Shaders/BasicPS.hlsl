89f15a52475f07637b589ebf76cd52156ecb96a9
yudaegam <61501369+yudaegam@users.noreply.github.com>
2023-05-06 00:27:11 +0900

    Add PBRInput structure for BRDF functions

--- sturdy-guacamole/Shaders/BasicPS.hlsl
+++ sturdy-guacamole/Shaders/BasicPS.hlsl
@@ -40,7 +40,7 @@ SamplerState Sampler_OcclusionTex  : register(s3);
 SamplerState Sampler_EmissiveTex  : register(s4);
 
 static const float PI = 3.141592;
-static const float Epsilon = 0.00001;
+static const float EPSILON = 0.00001;
 
 struct Light
 {
@@ -103,34 +103,56 @@ float3 GetNormal(PSInput psInput)
 	return n;
 }
 
+struct PBRInput
+{
+	float alpha;
+	float NdotL;
+	float NdotV;
+	float NdotH;
+	float HdotL;
+	float HdotV;
+};
+
+float3 Fresnel_schlick(float3 f0, float VdotH)
+{
+	return f0 + (1.0 - f0) * pow(1.0 - abs(VdotH), 5.0);
+}
 
 // The separable form of the Smith joint masking - shadowing function
-float G_Smith(float NdotL, float NdotV, float alpha)
+float G_Smith(PBRInput pbrInput)
 {
-	float alpha2 = alpha * alpha;
-	float shadowing = 2.0 * NdotL / (NdotL + sqrt(alpha2 + (1.0 - alpha2) * (NdotL * NdotL)));
-	float masking = 2.0 * NdotV / (NdotV + sqrt(alpha2 + (1.0 - alpha2) * (NdotV * NdotV)));
+	const float a2 = pbrInput.alpha * pbrInput.alpha;
+	const float NdotL = pbrInput.NdotL;
+	const float NdotV = pbrInput.NdotV;
+	const float HdotL = pbrInput.HdotL;
+	const float HdotV = pbrInput.HdotV;
+
+	const float shadowing = 2.0 * abs(NdotL) * (HdotL > 0.0) / max(abs(NdotL) + sqrt(a2 + (1.0 - a2) * (NdotL * NdotL)), EPSILON);
+	const float masking = 2.0 * abs(NdotV) * (HdotV > 0.0) / max(abs(NdotV) + sqrt(a2 + (1.0 - a2) * (NdotV * NdotV)), EPSILON);
 	return shadowing * masking;
 }
 
 // The Trowbridge-Reitz/GGX microfacet distribution
-float D_GGX(float NdotH, float alpha)
+float D_GGX(PBRInput pbrInput)
 {
-	float f = (NdotH * NdotH * (alpha * alpha - 1.0) + 1.0);
-	return alpha * alpha / (PI * f * f);
+	const float alpha = pbrInput.alpha;
+	const float NdotH = pbrInput.NdotH;
+
+	const float f = (NdotH * NdotH * (alpha * alpha - 1.0) + 1.0);
+	return alpha * alpha * (NdotH > 0.0) / max((PI * f * f), EPSILON);
 }
 
 // The specular reflection is a microfacet BRDF
 // microfacet BRDF = G * D / (4 * NdotL * NdotV)
-float3 Specular_brdf(float alpha, float NdotL, float NdotV, float NdotH)
+float3 Specular_brdf(PBRInput pbrInput)
 {
-	return G_Smith(NdotL, NdotV, alpha) * D_GGX(NdotH, alpha) / (4.0 * NdotL * NdotV);
+	return G_Smith(pbrInput) * D_GGX(pbrInput) / max(4.0 * abs(pbrInput.NdotL) * abs(pbrInput.NdotV), EPSILON);
 }
 
 // The diffuse reflection is a Lambertian BRDF
 float3 Diffuse_brdf(float3 color)
 {
-	return (1 / PI) * color;
+	return color / PI;
 }
 
 float4 main(PSInput psInput) : SV_Target0
@@ -139,7 +161,7 @@ float4 main(PSInput psInput) : SV_Target0
 	sampledColor.rgb = pow(sampledColor.rgb, float3(2.2, 2.2, 2.2));
 	sampledColor *= BaseColorFactor;
 
-	float3 baseColor = sampledColor.rgb;
+	const float3 baseColor = sampledColor.rgb;
 
 	float metallic = (HasMetallicRoughnessTex) ? MetallicRoughnessTex.Sample(Sampler_MetallicRoughnessTex, psInput.uv).b : 1.0;
 	metallic *= MetallicFactor;
@@ -147,47 +169,56 @@ float4 main(PSInput psInput) : SV_Target0
 	float roughness = (HasMetallicRoughnessTex) ? MetallicRoughnessTex.Sample(Sampler_MetallicRoughnessTex, psInput.uv).g : 1.0;
 	roughness *= RoughnessFactor;
 
-	float alpha = roughness * roughness;
+	const float alpha = roughness * roughness;
 
-	float3 n = GetNormal(psInput);
-	float3 v = normalize(ViewerPos - psInput.worldPos);
-	float NdotV = clamp(dot(n, v), 0.001, 1.0);
+	const float3 n = GetNormal(psInput);
+	const float3 v = normalize(ViewerPos - psInput.worldPos);
+	const float NdotV = dot(n, v);
 
 	const int num_lights = 1;
 	//float3 light_pos[num_lights] = { {0.0, 1000.0, 0.0}, { 1000.0, 0.0, 0.0 }, { -1000.0, 0.0, 0.0 } };
 	float3 light_pos[num_lights] = { {10.0, 0.0, 0.0} };
 	float4 color = float4(0.0, 0.0, 0.0, 1.0);
+
+	[unroll]
 	for (int i = 0; i < num_lights; ++i)
 	{
-		float3 l = normalize(light_pos[i] - psInput.worldPos);
-		float3 h = normalize(v + l);
+		const float3 l = normalize(light_pos[i] - psInput.worldPos);
+		const float3 h = normalize(v + l);
 
-		float NdotL = clamp(dot(n, l), 0.001, 1.0);
-		float NdotH = clamp(dot(n, h), 0.0, 1.0);
-		float VdotH = clamp(dot(v, h), 0.0, 1.0);
-		//float LdotH = clamp(dot(l, h), 0.0, 1.0);
+		PBRInput pbrInput;
+		pbrInput.alpha = alpha;
+		pbrInput.NdotL = dot(n, l);
+		pbrInput.NdotV = NdotV;
+		pbrInput.NdotH = dot(n, h);
+		pbrInput.HdotL = dot(h, l);
+		pbrInput.HdotV = dot(h, v);
 
-		float3 metal_brdf = Specular_brdf(alpha, NdotL, NdotV, NdotH)
-			* (baseColor.rgb + (float3(1.0, 1.0, 1.0) - baseColor.rgb) * pow(1.0 - VdotH, 5));
+		{
+			// f0 is 0.04 for dielectrics
+			float F_dieletric = 0.04 + (1.0 - 0.04) * pow(1.0 - abs(pbrInput.HdotV), 5);
 
-		float3 dielectric_brdf = lerp(Diffuse_brdf(baseColor.rgb), Specular_brdf(alpha, NdotL, NdotV, NdotH),
-			0.04 + (1.0 - 0.04) * pow(1.0 - VdotH, 5));
+			float3 metal_brdf = Specular_brdf(pbrInput) * Fresnel_schlick(baseColor.rgb, pbrInput.HdotV);
+			float3 dielectric_brdf = lerp(Diffuse_brdf(baseColor.rgb), Specular_brdf(pbrInput), F_dieletric);
 
-		// The BRDF of the metallic-roughness material is a linear interpolation of a metallic BRDF and a dielectric BRDF.
-		float3 material = lerp(dielectric_brdf, metal_brdf, metallic);
-		color.rgb += material;
+			// The BRDF of the metallic-roughness material is a linear interpolation of a metallic BRDF and a dielectric BRDF.
+			float3 material = lerp(dielectric_brdf, metal_brdf, metallic);
+			color.rgb += material;
+		}
 
 		// We can simplify the mix and arrive at the final BRDF for the material
-		//float3 c_diff = lerp(baseColor.rgb, float3(0.0, 0.0, 0.0), metallic);
-		//float3 f0 = lerp(float3(0.04, 0.04, 0.04), baseColor.rgb, metallic);
-		//float a = roughness * roughness;
+		//{
+		//	float3 c_diff = lerp(baseColor.rgb, float3(0.0, 0.0, 0.0), metallic);
+		//	float3 f0 = lerp(float3(0.04, 0.04, 0.04), baseColor.rgb, metallic);
+		//	float a = roughness * roughness;
 
-		//float3 F = f0 + (1 - f0) * pow(1 - VdotH, 0.5);
+		//	float3 F = f0 + (1 - f0) * pow(1 - abs(pbrInput.HdotV), 0.5);
 
-		//float3 f_diffuse = (1 - F) / PI * c_diff;
-		//float3 f_specular = F * Specular_brdf(alpha, NdotL, NdotV, NdotH);
-		//float3 material = f_diffuse + f_specular;
-		//color.rgb += material;
+		//	float3 f_diffuse = (1 - F) / PI * c_diff;
+		//	float3 f_specular = F * Specular_brdf(pbrInput);
+		//	float3 material = f_diffuse + f_specular;
+		//	color.rgb += material;
+		//}
 	}
 
 	if (HasOcclusionTex)
